f53d5ddeabd40fa49b588aef42405d3730d54416
CarboneGhosn <ghosn.dani@hotmail.com>
2025-03-26 21:17:19 -0300

    changed url

--- generatorQr.py
+++ generatorQr.py
@@ -4,7 +4,7 @@ qr = qrcode.QRCode(version=1,
                    error_correction=qrcode.constants.ERROR_CORRECT_L,
                    box_size=50,
                    border=1)
-qr.add_data("https://www.jw.org/pt/")
+qr.add_data("www.linkedin.com/in/daniel-cgc")
 qr.make(fit=True)
 
 img = qr.make_image(fill_color="orange", back_color="white")
